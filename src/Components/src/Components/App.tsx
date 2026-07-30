62f1b2f3f18853c64f64b352b2f61284652af70a
dannyyang4140 <danny.yang4140@gmail.com>
2026-07-30 16:53:22 +0800

    Create App component with CAdd App.tsx component with MUI baselinessBaseline and invitation

---
+++ src/Components/src/Components/App.tsx
@@ -0,0 +1,14 @@
+import '../Styles/App.css';
+import {CssBaseline} from '@mui/material';
+import ConferenceInvitation from './ConferenceInvitation';
+
+function App() {
+  return (
+    <>
+      <CssBaseline />
+      <ConferenceInvitation />
+    </>
+  );
+}
+
+export default App;
